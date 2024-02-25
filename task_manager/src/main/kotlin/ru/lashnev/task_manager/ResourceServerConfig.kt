7bb7ada92cc2b1ce9c005f0b08fc67c8a39dea30
a.lashnev <a.lashnev@qiwi.com>
2024-02-25 16:18:52 +0300

    OAuth server fix 1

--- task_manager/src/main/kotlin/ru/lashnev/task_manager/ResourceServerConfig.kt
+++ task_manager/src/main/kotlin/ru/lashnev/task_manager/ResourceServerConfig.kt
@@ -6,13 +6,14 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+
 @Configuration
 @EnableWebSecurity
 class ResourceServerConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.securityMatcher("/task_manager/**")
-            .authorizeHttpRequests { authorize -> authorize.anyRequest().hasAuthority("task_manager.read") }
+            .authorizeHttpRequests { authorize -> authorize.anyRequest().hasAuthority("SCOPE_task-manager.read") }
             .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
         return http.build()
     }
